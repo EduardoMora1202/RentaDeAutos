4006ff68f0b82857dc75a3329edf14619f641ba0
EduardoMora1202 <the.mora1202@gmail.com>
2024-04-19 20:35:35 -0600

    ejemplo

---
+++ Rent_Car/Rent_Car/Content/js/DetallesAutos.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+
+    var urlParams = new URLSearchParams(window.location.search);
+    var autoId = urlParams.get('id');
+
+    $.ajax({
+
+        url: 'http://localhost:4000/vehiculoDetallesID/' + autoId,
+        type: 'GET',
+        success: function (data) {
+            //console.log(data);
+            console.log(data.TipoCombustible);
+            $("#marca").text(data.Marca);
+            $("#modelo").text(data.Modelo);
+            $("#Capacidad_Pasajeros").text(data.Capacidad_Pasajeros);
+            $("#Capacidad_Maletero").text(data.Capacidad_Maletero);
+            $("#precio").text(data.PrecioAlquilerDia);
+            $("#TipoCombustible").text(data.TipoCombustible);
+        },
+        error: function (xhr, status, error) {
+            console.error("Error al obtener detalles del vehículo:", error);
+        }
+    });
+
+});
